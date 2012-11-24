ffb403d1a90fd0a7749b86a0683eadb7866a3da5
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-11-24 09:21:37 +0000

    domain model
    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@141 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- ETUS.DomainModel/DomainModel.cs
+++ ETUS.DomainModel/DomainModel.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Irony;
+using Irony.Ast;
+using Irony.Parsing;
+using Expressions;
 
 namespace ETUS.DomainModel
 {
@@ -14,25 +18,42 @@ namespace ETUS.DomainModel
 
     public class NamespaceUsing
     {
+        public string Name { get; set; }
     }
 
     public class NamespaceDeclaration
     {
+        public string Name { get; set; }
     }
 
     public abstract class Definition
     {
+        public string Name { get; set; }
     }
 
     public class PrefixDefinition
     {
+        public Expression Factor { get; set; }
     }
 
     public class UnitDefinition
     {
+        public QuantityDefinition Quantity { get; set; }
+        public Conversion Conversion { get; set; }
     }
 
     public class QuantityDefinition
     {
+        Expression Expression = new Expression.BinaryExpression(new Expression.Parameter(), new BinaryOperator.Add(), new Expression.Number());
+    }
+
+    public abstract class Conversion
+    {
+    }
+
+    public class SimpleConversion : Conversion
+    {
+        public Expression Value { get; set; }
+        public UnitExpression Unit { get; set; }
     }
 }
