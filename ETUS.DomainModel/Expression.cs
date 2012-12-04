7b598e250ae71ec1030f05010a7e50c0ce61e56a
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-12-04 05:15:27 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@158 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- ETUS.DomainModel/Expression.cs
+++ ETUS.DomainModel/Expression.cs
@@ -6,28 +6,27 @@ namespace ETUS.DomainModel.Expressions
 {
     public abstract class Expression
     {
-    }
+        public class Binary : Expression
+        {
+            public Expression Expr1 { get; set; }
+            public BinaryOperator Op { get; set; }
+            public Expression Expr2 { get; set; }
+        }
 
-    public class BinaryExpression : Expression
-    {
-        public Expression Expr1 { get; set; }
-        public BinaryOperator Op { get; set; }
-        public Expression Expr2 { get; set; }
-    }
+        public class Unary : Expression
+        {
+            public UnaryOperator Op { get; set; }
+            public Expression Expr { get; set; }
+        }
 
-    public class UnaryExpression : Expression
-    {
-        public UnaryOperator Op { get; set; }
-        public Expression Expr { get; set; }
-    }
+        public class Number<T> : Expression
+        {
+            public T Value;
+        }
 
-    public class NumberExpression<T> : Expression
-    {
-        public T Value;
-    }
-
-    public class ParameterExpression : Expression
-    {
+        public class Parameter : Expression
+        {
+        }
     }
 
     public enum BinaryOperator
