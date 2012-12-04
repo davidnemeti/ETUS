7cba7a34411b82965afc872880c79ec105b7b5a1
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-12-04 18:21:08 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@176 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- ETUS.DomainModel/Expression.cs
+++ ETUS.DomainModel/Expression.cs
@@ -11,17 +11,32 @@ namespace ETUS.DomainModel.Expressions
             public Expression Term1 { get; set; }
             public BinaryOperator Op { get; set; }
             public Expression Term2 { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("({0} {1} {2})", Term1, Op, Term2);
+            }
         }
 
         public class Unary : Expression
         {
             public UnaryOperator Op { get; set; }
             public Expression Term { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("({0} {1})", Op, Term);
+            }
         }
 
         public class Number<T> : Expression
         {
             public T Value;
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
         }
 
         public class Parameter : Expression
